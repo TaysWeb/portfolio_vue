84e837c5725323a74dcd767a33c3fc7b849a803b
TaysWeb <snelltararobin@gmail.com>
2023-08-07 18:31:49 +0100

    home stash

--- src/views/QualifView.vue
+++ src/views/QualifView.vue
@@ -8,7 +8,7 @@
             <h3 class="fw-bold mb-5 fs-1 text-decoration-underline">Resume <span>📌</span> </h3>
           </div>
       </div>
-<<<<<<< HEAD
+
       <div class="divider"></div>
 
       <button @click="getpost">Show</button>
@@ -16,9 +16,9 @@
       <div v-for="p in post" :key="p.id" >
           <h2>{{p.id}} - {{p.title}}</h2>
       </div>
-=======
+
       <div class="divider"></div>    
->>>>>>> 472fb45246979c96f11dd32ec5c0c2daa628823e
+
     </div>
     <div class="row">
       <eduComp :Education="Education"/>
@@ -34,28 +34,13 @@
 import eduComp from '../components/Edu-comp.vue'
 import EndComp from '../components/footer-comp.vue'
 export default {
- 
-<<<<<<< HEAD
-    components : { resumeComp, EndComp},
-    data() {
-      return {
-        post:{}
-      }
-    },
-    methods: {
-      getPost() {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(json =>this.post = data)
-      }
-    },
-=======
+
+
 
     components: {
       eduComp,EndComp,
     }
 
->>>>>>> 472fb45246979c96f11dd32ec5c0c2daa628823e
 }
 </script>
 <style >
